75d320bec0c51b28f55420a06f76f79c3826b9e7
Shamryk Artem <artem.shamryk.ki.2023@lpnu.ua>
2025-10-05 15:45:59 +0300

    готова лабораторна

--- LAB_01/src/main.java
+++ LAB_01/src/main.java
@@ -1,62 +1,62 @@
 import java.io.*;
 import java.util.*;
-/**
-* Клас Lab1 реалізує приклад програми до лабораторної роботи №1
-*/
-public class main
-{
-/**
-* Статичний метод main є точкою входу в програму
-* 
-* @param args
-* @throws FileNotFoundException
-* 
-*/
-public static void main(String[] args) throws FileNotFoundException
 
-{
-int nRows;
-char[][] arr;
-String filler;
-Scanner in = new Scanner(System.in);
-File dataFile = new File("MyFile.txt");
-PrintWriter fout = new PrintWriter(dataFile);
-System.out.print("Введіть розмір квадратної матриці: ");
-nRows = in.nextInt();
-in.nextLine();
-arr = new char[nRows][];
-for(int i = 0; i < nRows; i++)
-{
-arr[i]= new char[i+1];
-}
-System.out.print("\nВведіть символ-заповнювач: ");
-filler = in.nextLine();
-exit:
-for(int i = 0; i < nRows; i++)
-{
-for(int j = 0; j < i+1; j++)
-{
-if(filler.length() == 1)
-{
-arr[i][j] = (char) filler.codePointAt(0);
-System.out.print(arr[i][j] + " ");
-fout.print(arr[i][j] + " ");
-}
-else if (filler.length() == 0)
-{
-System.out.print("\nНе введено символ заповнювач");
-break exit;
-}
-else 
-{
-System.out.print("\nЗабагато символів заповнювачів");
-break exit;
-}
-}
-System.out.print("\n");
-fout.print("\n");
-}
-fout.flush();
-fout.close();
-}
+/**
+ * Клас Lab1_2 — фігура «пісочний годинник» (заповнені верх і низ)
+ */
+public class main {
+    public static void main(String[] args) throws FileNotFoundException {
+        int n;
+        char[][] arr;
+        char filler;
+
+        Scanner in = new Scanner(System.in);
+        File dataFile = new File("MyFile2.txt");
+        PrintWriter fout = new PrintWriter(dataFile);
+
+        System.out.print("Введіть розмір квадратної матриці (непарне число ≥ 3): ");
+        n = in.nextInt();
+        in.nextLine();
+
+        System.out.print("Введіть символ-заповнювач: ");
+        String s = in.nextLine();
+        if (s.length() != 1) {
+            System.out.println("Потрібно ввести рівно один символ!");
+            return;
+        }
+        filler = s.charAt(0);
+
+        arr = new char[n][n];
+
+        // Формування заповнених верхнього і нижнього трикутників
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // Верхня половина
+                if (i <= n / 2 && (j >= i && j < n - i)) {
+                    arr[i][j] = filler;
+                }
+                // Нижня половина
+                else if (i > n / 2 && (j >= n - i - 1 && j <= i)) {
+                    arr[i][j] = filler;
+                } 
+                // Інше — порожнеча
+                else {
+                    arr[i][j] = ' ';
+                }
+            }
+        }
+
+        // Вивід
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j]);
+                fout.print(arr[i][j]);
+            }
+            System.out.println();
+            fout.println();
+        }
+
+        fout.flush();
+        fout.close();
+    }
 }
